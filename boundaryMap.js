9b8f6fe5a3a48b6637ebff82037dc3ebd10a207c
tt50 <156944954+tt50@users.noreply.github.com>
2025-04-30 21:28:27 -0400

    added highlight

--- boundaryMap.js
+++ boundaryMap.js
@@ -5,13 +5,15 @@
         //d3.json("./numberOfVehiclesHousehold_ZipCode.json"),
         //d3.json("./meansOfTransporation_ZipCode.json")
     ]).then((data) => {
-
         const zipcodes_poly = data[0];
 
         const svg = d3.select("#boundaryMap");
         const width = +svg.attr("width");
         const height = +svg.attr("height");
 
+        // northeast philadelphia zipcodes
+        const northeast_zipcodes = new Set(["19111","19114","19115","19116","19120","19124","19149","19152","19154"]);
+        
         const projection = d3.geoAlbers()
             .center([-75.1652, 39.9526])  // Philadelphia, PA coordinates
             .rotate([0, 0])
@@ -21,17 +23,34 @@
 
         const path = d3.geoPath().projection(projection);
 
+        // zip code areas
         svg.append("g")
-            .selectAll("path")
-            .data(zipcodes_poly.features)
-            .join("path")
-            .attr("d", path)
-            .attr("fill", '#ccc')  
-            .attr("stroke", "black")
-            .attr("stroke-width", "1px");
+        .selectAll("path")
+        .data(zipcodes_poly.features)
+        .join("path")
+        .attr("d", path)
+        .attr("fill", feature => {
+            const zipCode = feature.properties.CODE;
+            return northeast_zipcodes.has(zipCode) ? "steelblue" : "#f0f0f0";
+        })
+        .attr("stroke", "#333")
+        .attr("stroke-width", 0.7)
+        .attr("stroke-opacity", 0.8)
+        .style("cursor", feature => { // pointer for northeast area
+            return northeast_zipcodes.has(feature.properties.CODE) ? "pointer" : "default";
+        })
+        .on("mouseover", function(event, feature) { // highlight northeast area
+            if (northeast_zipcodes.has(feature.properties.CODE)) {
+                d3.select(this).attr("fill", "orange");
+            }
+        })
+        .on("mouseout", function(event, feature) { // mouse out highlight northeast area
+            if (northeast_zipcodes.has(feature.properties.CODE)) {
+                d3.select(this).attr("fill", "steelblue");
+            }
+        });
 
     }).catch((error) => {
         console.error("Error loading data:", error);
     });
-
 })();
